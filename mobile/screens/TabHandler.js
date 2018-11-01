63f21b7038f4a5be67e81a477924fd8429d2d27b
lievertom <lievertom@gmail.com>
2018-10-31 22:53:46 -0300

    Deleting app integra

--- mobile/screens/TabHandler.js
+++ mobile/screens/TabHandler.js
@@ -1,92 +1,112 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
-    View
-} from 'react-native';
+    View,
+    Text,
+    StyleSheet
+} from "react-native";
+import { TabNavigator } from 'react-navigation'
+import Feed from './tab_navigator/car_defense/screens/Feed'
+import PublicNotifications from './tab_navigator/car_defense/screens/TabNavigator/PublicNotifications/PublicNotifications'
+import PrivateNotifications from './tab_navigator/car_defense/screens/TabNavigator/PrivateNotifications/PrivateNotifications'
+import RegisterCar from './tab_navigator/car_defense/screens/TabNavigator/RegisterCar/RegisterCar'
+import PrivateFeed from './tab_navigator/car_defense/screens/TabNavigator/PrivateFeed/PrivateFeed'
 import { Icon } from 'native-base';
-import { TabNavigator } from 'react-navigation';
-import TabHandlerVendasApp from './tab_navigator/vendas/TabHandlerVendasApp';
-import IndicaAiApp from './tab_navigator/indica_ai/IndicaAiApp';
-import RolesApp from './tab_navigator/roles/RolesApp';
-import Settings from './tab_navigator/settings/Settings';
-import CarDefenseTabHandler from './tab_navigator/car_defense/CarDefenseTabHandler';
+
 
 const TabHandler = new TabNavigator({
-    Roles: {
-        screen: RolesApp,
+    Feed: {
+        screen: Feed,
         navigationOptions: {
-            tabBarLabel: 'Eventos',
-            headerLeft: null,
+            tabBarLabel: 'Feed',
             tabBarIcon: ({ focused }) => (
                 <Icon
-                    name="md-star"
-                    style={{ color: focused ? '#1CBD24' : '#5A5A5A' }}
+                    type='FontAwesome'
+                    name="home"
+                    style={{ color: '#5c68c3', fontSize: 16 }}
                 />
-            )
+            ),
+            headerLeft: null,
         }
     },
-    Vendas: {
-        screen: TabHandlerVendasApp,
+
+    PrivateNotifications: {
+        screen: PrivateNotifications,
         navigationOptions: {
-            tabBarLabel: 'Vendas',
+            tabBarLabel: 'Notificação',
             tabBarIcon: ({ focused }) => (
                 <Icon
-                    name="md-cart"
-                    style={{ color: focused ? '#0EAC6F' : '#5A5A5A' }}
+                    type='FontAwesome'
+                    name="send-o"
+                    style={{ color: '#5c68c3', fontSize: 15 }}
                 />
-            )
+            ),
+            headerLeft: null,
         }
     },
-    IndicaAi: {
-        screen: IndicaAiApp,
+    PublicNotifications: {
+        screen: PublicNotifications,
         navigationOptions: {
-            tabBarLabel: 'IndicaAi',
-            headerLeft: null,
-            tabBarIcon: ({ focused }) => (
-                <Icon 
-                    name="md-locate"
-                    style={{ color: focused ? '#0AACCC' : '#5A5A5A' }}
-                />
-            )
-        }
-    },
-    CarDefense: {
-        screen: CarDefenseTabHandler,
-        navigationOptions: {
-            tabBarLabel: 'CarDefense',
-            headerLeft: null,
+            tabBarLabel: 'Alerta',
             tabBarIcon: ({ focused }) => (
                 <Icon
-                    name="md-car"
-                    style={{ color: focused ? '#5C68C3' : '#5A5A5A' }}
+                    type='FontAwesome'
+                    name="warning"
+                    style={{ color: '#5c68c3', fontSize: 15 }}
                 />
-            )
+            ),
+            headerLeft: null,
         }
     },
-    Settings: {
-        screen: Settings,
+    RegisterCar: {
+        screen: RegisterCar,
         navigationOptions: {
-            tabBarLabel: 'Configurações',
-            headerLeft: null,
+            tabBarLabel: 'Carros',
             tabBarIcon: ({ focused }) => (
                 <Icon
-                    name="md-settings"
-                    style={{ color: focused ? '#BD1C5F' : '#5A5A5A' }}
+                    type='FontAwesome'
+                    name="car"
+                    style={{ color: '#5c68c3', fontSize: 15 }}
+
                 />
-            )
+            ),
+            headerLeft: null,
         }
     },
-}, {
-    tabBarOptions: {
-        showIcon: true,
-        showLabel: false,
-        style: {
-            backgroundColor: '#171717',
+    PrivateFeed: {
+        screen: PrivateFeed,
+        navigationOptions: {
+            tabBarLabel: 'Feed Privado',
+            tabBarIcon: ({ focused }) => (
+                <Icon
+                    type='FontAwesome'
+                    name="bell-o"
+                    style={{ color: '#5c68c3', fontSize: 15 }}
+                />
+            ),
+            headerLeft: null,
+        }
+    },
+},
+    {
+        tabBarOptions: {
+            showLabel: true,
+            showIcon: true,
+            swipeEnabled: true,
+            activeTintColor: 'black',
+            inactiveTintColor: '#540b71',
+            labelStyle: {
+                fontSize: 5,
+            },
+            tabStyle: {
+                height: 65,
+            },
+            style: {
+                backgroundColor: '#fff',
+            },
+            animationEnabled: true,
         },
-        tabStyle: {
-            height: 60,
-        },
-    },
-    animationEnabled: true,
-});
+
+    });
+
 
 export default TabHandler;
