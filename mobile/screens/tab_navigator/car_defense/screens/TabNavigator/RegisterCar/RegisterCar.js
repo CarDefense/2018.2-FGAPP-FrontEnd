0d28b17b4cf5e5ec1bc522e511e76c7aa8ee2165
bielrossi15 <bielrossiborba@gmail.com>
2018-11-05 19:40:03 -0200

    Refactoring register car functions and style

--- mobile/screens/tab_navigator/car_defense/screens/TabNavigator/RegisterCar/RegisterCar.js
+++ mobile/screens/tab_navigator/car_defense/screens/TabNavigator/RegisterCar/RegisterCar.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import { TextField } from 'react-native-material-textfield';
-import { View, Text, TouchableOpacity, ScrollView, StyleSheet, Alert, FlatList, RefreshControl } from 'react-native';
+import { View, Text, TouchableOpacity, ScrollView, StyleSheet, Alert, Image, RefreshControl } from 'react-native';
 // import Expo from 'expo'
 // import { Permissions, Notifications } from 'expo'
 import { CAR_API } from '../const/Const'
+import { Icon } from "native-base";
 
 
 // var tk
@@ -108,7 +109,7 @@ export default class RegisterCar extends Component {
   async componentDidMount() {
     this.setState({ hasError: false, errorMessage: '' })
     const { state } = this.props.navigation;
-    var id = state.params ? (state.params.user.id ? state.params.user.id : state.params.user.user_id ) : undefined;
+    var id = state.params ? (state.params.user.id ? state.params.user.id : state.params.user.user_id) : undefined;
     let link = CAR_API + '/car/?token=' + id
 
     return fetch(link)
@@ -127,14 +128,6 @@ export default class RegisterCar extends Component {
         console.error(error);
       });
   }
-
-  _onRefresh = () => {
-    this.setState({ refreshing: true });
-    this.componentDidMount().then(() => {
-      this.setState({ refreshing: false });
-    });
-  }
-
 
   onPressButton = () => {
     let errors = {};
@@ -184,8 +177,8 @@ export default class RegisterCar extends Component {
     if (errorPlate == false) {
 
       const { state } = this.props.navigation;
-      var id = state.params ? (state.params.user.id ? state.params.user.id : state.params.user.user_id ) : undefined;
-      
+      var id = state.params ? (state.params.user.id ? state.params.user.id : state.params.user.user_id) : undefined;
+
       const url = CAR_API + '/validate_car/' //cars db models url
 
       let car = JSON.stringify({
@@ -228,75 +221,80 @@ export default class RegisterCar extends Component {
     let { plate = 'text' } = data;
     let { model = 'text' } = data;
     let { color = 'text' } = data;
+    const { state } = this.props.navigation;
+    var user = state.params ? (state.params.user.name ? state.params.user.name : state.params.user.username) : undefined;
+
 
     return (
-      <ScrollView
-        refreshControl={
-          <RefreshControl
-            refreshing={this.state.refreshing}
-            onRefresh={this._onRefresh}
-          />
-        }
-      >
-        <View style={styles.container}>
-          <Text style={styles.header}> Cadastro</Text>
-          <TextField
-            ref={this.plateRef}
-            value={data.plate}
-            autoCorrect={false}
-            enablesReturnKeyAutomatically={true}
-            onFocus={this.onFocus}
-            onChangeText={(plate) => this.setState({ plate })}
-            // onChangeText={this.onChangeText}
-            // onSubmitEditing={this.onSubmitPLate}
-            returnKeyType='next'
-            label='Placa'
-            tintColor="#760f9f"
-            underlineColorAndroid="transparent"
-            maxLength={8}
-            autoCapitalize="characters"
-            error={errors.plate}
-          />
-          <TextField
-            ref={this.modelRef}
-            value={data.model}
-            autoCorrect={false}
-            enablesReturnKeyAutomatically={true}
-            onFocus={this.onFocus}
-            onChangeText={(model) => this.setState({ model })}
-            // onChangeText={this.onChangeText}
-            // onSubmitEditing={this.onSubmitModel}
-            returnKeyType='next'
-            label='Modelo'
-            tintColor="#760f9f"
-          />
-          <TextField
-            ref={this.colorRef}
-            value={data.color}
-            autoCorrect={false}
-            enablesReturnKeyAutomatically={true}
-            onFocus={this.onFocus}
-            onChangeText={(color) => this.setState({ color })}
-            // onChangeText={this.onChangeText}
-            // onSubmitEditing={this.onSubmitMessage}
-            returnKeyType='next'
-            label='Cor'
-            tintColor="#760f9f"
-          />
+      <View style={{ backgroundColor: "#8bd4da", flex: 1 }}>
+        <View style={styles.header}>
+          <View style={styles.headerContent}>
+            <Image style={styles.avatar}
+              source={{ uri: 'http://cardefense2.eastus.cloudapp.azure.com:8002/media/ssssssssssssssssssssssssssss.png' }}
+            />
+            <Text style={styles.name}>Olá, {user}! Cadastre seus carros!</Text>
+          </View>
         </View>
+        <TextField
+          ref={this.plateRef}
+          value={data.plate}
+          autoCorrect={false}
+          enablesReturnKeyAutomatically={true}
+          onFocus={this.onFocus}
+          onChangeText={(plate) => this.setState({ plate })}
+          // onChangeText={this.onChangeText}
+          // onSubmitEditing={this.onSubmitPLate}
+          returnKeyType='next'
+          label='Placa'
+          tintColor="white"
+          underlineColorAndroid="transparent"
+          maxLength={8}
+          autoCapitalize="characters"
+          error={errors.plate}
+          textColor="white"
+        />
+        <TextField
+          ref={this.modelRef}
+          value={data.model}
+          autoCorrect={false}
+          enablesReturnKeyAutomatically={true}
+          onFocus={this.onFocus}
+          onChangeText={(model) => this.setState({ model })}
+          // onChangeText={this.onChangeText}
+          // onSubmitEditing={this.onSubmitModel}
+          returnKeyType='next'
+          label='Modelo'
+          tintColor="white"
+          textColor="white"
+        />
+        <TextField
+          ref={this.colorRef}
+          value={data.color}
+          autoCorrect={false}
+          enablesReturnKeyAutomatically={true}
+          onFocus={this.onFocus}
+          onChangeText={(color) => this.setState({ color })}
+          // onChangeText={this.onChangeText}
+          // onSubmitEditing={this.onSubmitMessage}
+          returnKeyType='next'
+          label='Cor'
+          tintColor="white"
+          textColor="white"
+
+        />
         <View style={styles.container1}>
           <TouchableOpacity
             style={styles.button}
-            color="#5c68c3"
+            color="white"
             onPress={this.onPressButton}
             containerViewStyle={{ width: '40%' }}
           >
-            <Text style={{ color: 'white' }} >Cadastrar</Text>
+            <Text style={{ color: '#8bd4da', fontWeight: '800', fontSize: 15 }} >Cadastrar</Text>
           </TouchableOpacity>
           {/* {this.state.registered ? <Text style={{ flexDirection: 'row', justifyContent: 'center', color: '#5c68c3', marginTop: 20 }}>{this.state.registerMessage}</Text> : null } */}
 
         </View>
-      </ScrollView>
+      </View>
     );
   }
 }
@@ -307,16 +305,9 @@ const styles = StyleSheet.create({
     paddingRight: 16
   },
   container1: {
-    marginTop: 80,
+    marginTop: 13,
     flexDirection: 'row',
     justifyContent: 'center'
-  },
-  header: {
-    color: '#760f9f',
-    textAlign: 'center',
-    fontWeight: '200',
-    fontSize: 50,
-    marginTop: 25
   },
   header2: {
     color: '#760f9f',
@@ -327,7 +318,7 @@ const styles = StyleSheet.create({
     fontSize: 30,
   },
   button: {
-    backgroundColor: "#760f9f",
+    backgroundColor: "white",
     borderRadius: 15,
     height: 40,
     width: 120,
@@ -353,5 +344,35 @@ const styles = StyleSheet.create({
   text1: {
     color: "#760f9f",
     fontWeight: '200',
-  }
+  },
+  avatar: {
+    width: 170,
+    height: 170,
+    borderRadius: 63,
+    borderWidth: 7,
+    borderColor: "white",
+    marginBottom: 10,
+  },
+  name: {
+    fontSize: 18,
+    color: "white",
+    fontWeight: '800',
+  },
+  headerContent: {
+    padding: 30,
+    alignItems: 'center',
+  },
+  box: {
+    color: 'white'
+  },
+  header: {
+    backgroundColor: "#8bd4da",
+  },
+  userInfo: {
+    fontSize: 15,
+    color: "white",
+    fontWeight: '800',
+    marginTop: 30,
+    marginBottom: 15
+},
 });
