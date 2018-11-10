28f434f46ae97da7748b163ce2b322dc71734af8
lievertom <lievertom@gmail.com>
2018-11-10 18:48:53 -0200

    Testing compoentWillUnMount of feed screen

--- mobile/__tests__/Function-test.js
+++ mobile/__tests__/Function-test.js
@@ -2,6 +2,7 @@ import 'react-native'
 import React from 'react'
 import Feed from '../screens/tab_navigator/car_defense/screens/Feed'
 import renderer from 'react-test-renderer'
+import * as register from '../screens/tab_navigator/car_defense/screens/Feed'
 
 // it('Function getFeedInfo', () => {
 //     let feedData = renderer.create(<Feed/>).getInstance()
@@ -17,9 +18,33 @@ it('Function onRefresh', () => {
 
     feedData._onRefresh()
 
-    expect(feedData._onRefresh(true)).toEqual()
+    expect(feedData._onRefresh(true)).toEqual(undefined)
     
 })
+
+// componentWillUnmount() {
+//     this.listener && Expo.Notifications.addListener(this.listen);
+//   }
+it('Function componentWillUnmount', () => {
+    let feedData = renderer.create(<Feed/>).getInstance()
+
+    feedData.componentWillUnmount()
+
+    expect(feedData.componentWillUnmount(true)).toEqual(undefined)
+    
+})
+
+// it('async function register', async () => {
+//     register()
+//     // if (status != 'granted') {
+//     //     alert('You need to enable permissions in settings');
+//     //     return;
+//     // }
+
+//     const value = await Expo.Notifications.getExpoPushTokenAsync();
+//     tk = value;
+//     expect(tk).toBe()
+// })
 
 // // it('Function sendIdData', () => {
 // //     let feedData = renderer.create(<Feed/>).getInstance()
