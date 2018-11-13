d32488cd5ee4ffb642c98b528bf2abc94c345ff6
bielrossi15 <bielrossiborba@gmail.com>
2018-11-13 00:31:48 -0200

    Changing const CAR_API value
    Co-authored-by: Joao Gabriel Rossi <bielrossiborba@gmail.com>
    Co-authored-by: Joao Gabriel Antunes <jg.antunes.medeiros@gmail.com>

--- mobile/screens/tab_navigator/car_defense/screens/TabNavigator/const/Const.js
+++ mobile/screens/tab_navigator/car_defense/screens/TabNavigator/const/Const.js
@@ -2,6 +2,6 @@ const PROFILE_API = 'http://cardefense3.eastus.cloudapp.azure.com:8005'
 
 const NOTIFICATIONS_API = 'http://cardefense3.eastus.cloudapp.azure.com:8002'
 
-const CAR_API = 'http://192.168.0.9:8003'
+const CAR_API = 'http://cardefense3.eastus.cloudapp.azure.com:8003'
 
 export { PROFILE_API, CAR_API, NOTIFICATIONS_API }
